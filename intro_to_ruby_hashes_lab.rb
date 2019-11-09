f06aa908575617798252b21f195c7c41c7c9523c
Kizito Njoku <nkizito57@gmail.com>
2019-11-09 04:49:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,9 +25,9 @@ monopoly = {
       
     },
     :rent_in_dollars => {
-      :one_piece_owned => 25
-      :two_piece_owned => 50
-      :three_piece_owned => 100
+      :one_piece_owned => 25,
+      :two_piece_owned => 50,
+      :three_piece_owned => 100,
       :four_piece_owned => 200
     }
 }}
@@ -47,9 +47,9 @@ def monopoly_with_third_tier
       
     },
     :rent_in_dollars => {
-      :one_piece_owned => 25
-      :two_piece_owned => 50
-      :three_piece_owned => 100
+      :one_piece_owned => 25,
+      :two_piece_owned => 50,
+      :three_piece_owned => 100,
       :four_piece_owned => 200
     }
 }}
@@ -76,9 +76,9 @@ def monopoly_with_fourth_tier
       
     },
     :rent_in_dollars => {
-      :one_piece_owned => 25
-      :two_piece_owned => 50
-      :three_piece_owned => 100
+      :one_piece_owned => 25,
+      :two_piece_owned => 50,
+      :three_piece_owned => 100,
       :four_piece_owned => 200
     }
 }}
