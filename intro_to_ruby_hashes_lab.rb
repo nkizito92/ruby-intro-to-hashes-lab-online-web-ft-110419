06121c5413035a44b1228d3b56fcf2ea73d71f2c
Kizito Njoku <nkizito57@gmail.com>
2019-11-09 04:52:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,13 +17,7 @@ def monopoly_with_second_tier
 monopoly = {
   :railroads => {
     :pieces => 4,
-    :names => {
-      :reading_railroad => {},
-      :b_and_o_railroad => {},
-      :pennsylvania_railroad => {},
-      :shortline => {}
-      
-    },
+    :names => {},
     :rent_in_dollars => {}
 }}
 monopoly
