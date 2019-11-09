cc59398a12fa5410d77a210b5cf698dcac4432be
Kizito Njoku <nkizito57@gmail.com>
2019-11-09 04:41:40 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,7 +18,12 @@ monopoly = {
   :railroads => {
     :pieces => 4,
     :names => {},
-    :rent_in_dollars => {}
+    :rent_in_dollars => {
+      :one_piece_owned => 25
+      :two_piece_owned => 50
+      :three_piece_owned => 100
+      :four_piece_owned => 200
+    }
 }}
 monopoly
 end
